73f578fed2262d671829ba749d89e9c449d1b9e4
Mark Kalinovits <mkalinovits@chemaxon.com>
2017-11-18 23:05:18 +0100

    Updating chanel component with handling multiple inputs

--- frontend/src/app/chanel/chanel.component.ts
+++ frontend/src/app/chanel/chanel.component.ts
@@ -7,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChanelComponent implements OnInit {
 
-  data = {
-    url: 'http://www.origo.hu/itthon/20171118-volt-telitalalatos-az-otoslotton.html',
-    title: 'Ha eltalálta ezt az 5 számot, akkor máris milliárdos',
-    abstract: 'Volt telitalálatos szelvény az ötös lottón'
-  };
+  items = [
+    {
+      url: 'http://www.origo.hu/itthon/20171118-volt-telitalalatos-az-otoslotton.html',
+      title: 'Ha eltalálta ezt az 5 számot, akkor máris milliárdos',
+      abstract: 'Volt telitalálatos szelvény az ötös lottón'
+    },
+    {
+      url: 'http://www.origo.hu/itthon/20171117-ajandekba-adjak-a-sztarok-a-szivuket-advent-idejen.html',
+      title: 'Ajándékba adják a szívüket',
+      abstract: 'Énekesek, sportolók és tévések készítenek mézeskalácsszíveket, ezzel segítik az Ökumenikus Segélyszervezet kampányát.'
+    },
+    {
+      url: 'http://www.origo.hu/itthon/20171116-alkoholista-anyja-hosszu-napokra-egy-szekrenybe-zarta-lopott-csalt-es-' +
+      'prostitualtkent-is-dolgozott.html',
+      title: 'A kedves ápolónő, aki a kertben ásta el a hullákat',
+      abstract: 'Dorothea Puente 9 embert mérgezett meg.'
+    }
+  ];
 
   constructor() { }
 
